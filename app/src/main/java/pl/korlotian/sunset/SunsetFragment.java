1aa30ac16117a66fc850bf93d5206e6f67c48efa
Maria Korlotian <maria.stoklosa@netguru.pl>
2017-05-14 17:57:46 +0200

    Add animation

--- app/src/main/java/pl/korlotian/sunset/SunsetFragment.java
+++ app/src/main/java/pl/korlotian/sunset/SunsetFragment.java
@@ -1,5 +1,6 @@
 package pl.korlotian.sunset;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,6 +9,10 @@ import android.view.ViewGroup;
 
 
 public class SunsetFragment extends Fragment {
+
+    private View mSceneView;
+    private View mSunView;
+    private View mSkyView;
 
     public static SunsetFragment newInstance() {
         return new SunsetFragment();
@@ -18,6 +23,28 @@ public class SunsetFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sunset, container, false);
 
+        mSceneView = view;
+        mSunView = view.findViewById(R.id.sun);
+        mSkyView = view.findViewById(R.id.sky);
+
+        mSceneView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startAnimation();
+            }
+        });
+
         return view;
+    }
+
+    private void startAnimation() {
+        float sunYStart = mSunView.getTop();
+        float sunYEnd = mSunView.getHeight();
+
+        ObjectAnimator heightAnimator = ObjectAnimator
+                .ofFloat(mSunView, "y", sunYStart, sunYEnd)
+                .setDuration(3000);
+
+        heightAnimator.start();
     }
 }
